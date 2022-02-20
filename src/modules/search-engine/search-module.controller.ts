b78e514473205cf303fd681bf2cc8c52a82ea33f
Deybith Simijaca <steven.simijaca.beltran@gmail.com>
2022-02-20 21:57:06 +0000

    chore(business): using core

--- src/modules/search-engine/search-module.controller.ts
+++ src/modules/search-engine/search-module.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from "@core/common";
+import { Controller, Get, HttpCode, Post } from "@core/common";
+import { HttpStatus } from "@core/common/enums";
 
 @Controller('search')
-export class SearchEngineController {}
+export class SearchEngineController {
+  
+  @Post('/post')
+  @HttpCode(HttpStatus.CREATED)
+  getHello(): string {
+    return 'Testtt';
+  }
+
+  @Get()
+  other(): string {
+    return 'other path '
+  } 
+}
